a4c1053545cebf8e184592d4d0a91f001bec2203
Surendra Kumar <sk2812283@gmail.com>
2018-08-06 02:52:48 +0530

    use prebuilt kernel & dt.img

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,17 +18,19 @@ art_dont_bother=true
 # Assert
 TARGET_OTA_ASSERT_DEVICE := j3ltespr,SM-J320P,SM-J320P,MSM8916
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK      := device/samsung/j3ltespr/mkbootimg.mk
+#BOARD_CUSTOM_BOOTIMG_MK      := device/samsung/j3ltespr/mkbootimg.mk
 BOARD_KERNEL_CMDLINE         := console=null androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci androidboot.selinux=permissive
 BOARD_KERNEL_BASE            := 0x80000000
 BOARD_RAMDISK_OFFSET         := 0x02000000
 BOARD_KERNEL_TAGS_OFFSET     := 0x01e00000
 BOARD_KERNEL_SEPARATED_DT    := true
 BOARD_KERNEL_PAGESIZE        := 2048
-TARGET_KERNEL_SOURCE         := kernel/samsung/j3ltespr
-TARGET_KERNEL_CONFIG         := msm8916_sec_defconfig
-TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_j3lte_spr_defconfig
-TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
+BOARD_MKBOOTIMG_ARGS 	     := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000 --dt device/samsung/j3ltespr/dt.img
+#TARGET_KERNEL_SOURCE         := kernel/samsung/j3ltespr
+#TARGET_KERNEL_CONFIG         := msm8916_sec_defconfig
+#TARGET_KERNEL_VARIANT_CONFIG := msm8916_sec_j3lte_spr_defconfig
+#TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
+TARGET_PREBUILT_KERNEL	     := device/samsung/j3ltespr/kernel
 
 # PARTITION SIZE
 BOARD_BOOTIMAGE_PARTITION_SIZE := 13312000
